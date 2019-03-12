845f4a29bed213e9d2c7a8069974e5c9b6268609
KlepaczPrzemyslaw <przemyslaw.klepacz@si-consulting.pl>
2019-03-12 15:04:46 +0100

    OldHttp

--- Frontend/src/app/header/header.component.ts
+++ Frontend/src/app/header/header.component.ts
@@ -1,8 +1,22 @@
+import { HttpService } from './../shared/http.service';
 import { Component } from '@angular/core';
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  constructor(private httpSerice: HttpService) { }
+
+  onSaveData() {
+    this.httpSerice.storeRecipes()
+    .subscribe((response: Response) => {
+      console.log(response);
+    });
+  }
+
+  fetchData() {
+    this.httpSerice.getRecipes();
+  }
 }
