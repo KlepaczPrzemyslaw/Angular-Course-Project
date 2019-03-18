890182625aaf372c6a718c104a8f228e7e68d629
KlepaczPrzemyslaw <przemyslaw.klepacz@si-consulting.pl>
2019-03-18 15:19:08 +0100

    New_HttpClient

--- Frontend/src/app/core/header/header.component.ts
+++ Frontend/src/app/core/header/header.component.ts
@@ -1,7 +1,7 @@
 import { AuthService } from './../../auth/auth.service';
 import { HttpService } from './../../shared/http.service';
 import { Component } from '@angular/core';
-import { Response } from '@angular/http';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +12,7 @@ export class HeaderComponent {
 
   onSaveData() {
     this.httpSerice.storeRecipes()
-    .subscribe((response: Response) => {
+    .subscribe((response: HttpEvent<Object>) => {
       console.log(response);
     });
   }
